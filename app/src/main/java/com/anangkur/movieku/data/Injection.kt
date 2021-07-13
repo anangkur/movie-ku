15c2ce70ec9aeca28185f6a942f28380a2081830
anangkur <anangk97@gmail.com>
2021-07-13 18:25:59 +0700

    feat(app): add provideRepository, provideApiService, provideLocalDataSource, provideRemoteDataSource, provideSharedPreferencesHelper in Injection.kt

--- app/src/main/java/com/anangkur/movieku/data/Injection.kt
+++ app/src/main/java/com/anangkur/movieku/data/Injection.kt
@@ -1,10 +1,53 @@
 package com.anangkur.movieku.data
 
 import android.content.Context
+import com.anangkur.movieku.BuildConfig
 import com.anangkur.movieku.data.local.LocalDataSource
 import com.anangkur.movieku.data.local.SharedPreferenceHelper
+import com.anangkur.movieku.data.remote.ApiService
 import com.anangkur.movieku.data.remote.RemoteDataSource
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object Injection {
-    fun provideRepository(context: Context) = Repository(LocalDataSource(SharedPreferenceHelper(context), context), RemoteDataSource)
+
+    fun provideRepository(
+        context: Context
+    ) = Repository(
+        provideLocalDataSource(context),
+        provideRemoteDataSource(context)
+    )
+
+    fun provideApiService(
+        context: Context
+    ): ApiService = Retrofit.Builder()
+        .baseUrl(BuildConfig.baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addInterceptor(ChuckerInterceptor(context))
+                .build()
+        )
+        .build()
+        .create(ApiService::class.java)
+
+    private fun provideLocalDataSource(
+        context: Context
+    ) = LocalDataSource(
+        provideSharedPreferencesHelper(context),
+        context
+    )
+
+    private fun provideRemoteDataSource(
+        context: Context
+    ) = RemoteDataSource(provideApiService(context))
+
+    private fun provideSharedPreferencesHelper(context: Context) = SharedPreferenceHelper(context)
 }
